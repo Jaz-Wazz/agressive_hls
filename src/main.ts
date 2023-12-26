5dbbf05970b02bbf1361df54473357cfe9cf8f5c
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-12-26 19:36:57 +0300

    Many type deduse experiments.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import Hls from "hls.js";
-import { Loaderx } from "./agressive_hls";
+import { CustomLoader } from "./agressive_hls";
 
 window.onload = () =>
 {
@@ -7,7 +7,7 @@ window.onload = () =>
 
 	if(player instanceof HTMLVideoElement)
 	{
-		let hls = new Hls({loader: Loaderx, enableWorker: true});
+		let hls = new Hls({fLoader: CustomLoader, enableWorker: true});
 		hls.loadSource('http://ia800509.s3dns.us.archive.org/moddychat_2023.07.05_12.05_test/index-dvr.m3u8');
 		hls.attachMedia(player);
 	}
