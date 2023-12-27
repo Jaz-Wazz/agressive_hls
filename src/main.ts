d7807818120ee2ee1df1732f58a429d12bfdbbc6
Jaz-Wazz <inc.herobrine275@gmail.com>
2023-12-27 19:16:52 +0300

    Move loader implementation to "main.ts".

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
-import Hls from "hls.js";
-import { CustomLoader } from "./agressive_hls";
+import Hls, { FragmentLoaderContext, HlsConfig, Loader, LoaderCallbacks, LoaderConfiguration, LoaderContext } from "hls.js";
+
+class CustomLoader extends (<new (confg: HlsConfig) => Loader<FragmentLoaderContext>> Hls.DefaultConfig.loader)
+{
+	constructor(config: HlsConfig)
+	{
+		super(config);
+		console.log("construct");
+	}
+
+	async load(context: FragmentLoaderContext, config: LoaderConfiguration, callbacks: LoaderCallbacks<LoaderContext>)
+	{
+		console.log("load");
+		let response = await fetch(context.url);
+		let buffer = await response.arrayBuffer();
+		context.responseType = "arraybuffer";
+		callbacks.onSuccess({url: context.url, data: buffer}, this.stats, context, null);
+	}
+
+	abort(): void
+	{
+		console.log("abort");
+	}
+}
 
 window.onload = () =>
 {
