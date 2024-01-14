ac81c090d8682376ce81d599b36f14f143c0d777
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-01-14 04:23:05 +0300

    Change test stream url.

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,6 @@ window.onload = () =>
 		hls.startLoad(window.location.hash.length > 0 ? parseInt(window.location.hash.slice(1)) : -1);
 	});
 
-	hls.loadSource('http://ia601703.s3dns.us.archive.org/76b6526e/index-muted-Y4POEUXVTL.m3u8');
+	hls.loadSource('http://ia601700.s3dns.us.archive.org/659b8ee7/index-muted-YK4PWD7N60.m3u8');
 	hls.attachMedia(player);
 };
