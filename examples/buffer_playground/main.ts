af59ccc96cf475b697a53e118014b9b73434243a
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-01-14 12:34:43 +0300

    Realize "buffer_playground" example.

--- examples/buffer_playground/main.ts
+++ examples/buffer_playground/main.ts
@@ -1,27 +1,24 @@
 import Hls from "hls.js";
 import { AgressiveHls } from "agressive_hls";
 
+declare global
+{
+	interface Window { subscribe: () => void; }
+}
+
 window.onload = () =>
 {
-	let player		= document.getElementsByTagName("video")[0];
 	let text_area	= document.getElementsByTagName("textarea")[0];
-
-	let buffer	= new AgressiveHls.Buffer;
-	let hls		= new Hls({fLoader: buffer.make_loader(), enableWorker: true, autoStartLoad: false});
-
-	player.ontimeupdate = () =>
-	{
-		let time = Math.round(player.currentTime).toString();
-		if(window.location.hash != time) window.location.hash = time;
-	};
+	let buffer		= new AgressiveHls.Buffer;
+	let hls			= new Hls({fLoader: buffer.make_loader(), enableWorker: true, autoStartLoad: false});
 
 	buffer.on_stats_update = (content) => text_area.textContent = content;
-	hls.on(Hls.Events.LEVEL_LOADED, (event, data) =>
-	{
-		buffer.playlist = data.details.fragments;
-		hls.startLoad(window.location.hash.length > 0 ? parseInt(window.location.hash.slice(1)) : -1);
-	});
-
+	hls.on(Hls.Events.LEVEL_LOADED, (event, data) => buffer.playlist = data.details.fragments);
 	hls.loadSource('http://ia601700.s3dns.us.archive.org/659b8ee7/index-muted-YK4PWD7N60.m3u8');
-	hls.attachMedia(player);
+
+	window.subscribe = () =>
+	{
+		let index = parseInt(document.getElementsByTagName("input")[0].value);
+		buffer.subscribe(index, (buffer) => console.warn("Requested segment", index, "getted, with", buffer.byteLength, "bytes."));
+	};
 };
