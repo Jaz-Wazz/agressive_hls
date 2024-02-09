ae8f8a77cc391339abb38e5a730a88a681829195
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-02-09 10:18:37 +0300

    Add basic server mode with refactor "esbuild.mjs".

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,9 +1,20 @@
 import * as esbuild from "esbuild";
 
-await esbuild.build(
+let opts =
 {
 	entryPoints: ["examples/browser_player/main.ts", "examples/buffer_playground/main.ts"],
 	outdir: "build",
 	bundle: true,
 	sourcemap: true
-});
+};
+
+if(process.argv[2] == "--build")
+{
+	await esbuild.build(opts);
+}
+
+if(process.argv[2] == "--server")
+{
+	let ctx = await esbuild.context(opts);
+	await ctx.serve({servedir: "build"});
+}
