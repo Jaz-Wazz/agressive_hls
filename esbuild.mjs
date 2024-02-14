16f1f2071c979b5d92b05ba8e8f52dd74fd1a776
Jaz-Wazz <inc.herobrine275@gmail.com>
2024-02-14 16:16:58 +0300

    Refactor build system and command interface.

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,41 +1,49 @@
 import * as esbuild from "esbuild";
+import * as process from "node:process";
 
-const opts =
+const configurations =
 {
-	entryPoints:
-	[
-		"examples/browser_player/main.html",
-		"examples/browser_player/main.css",
-		"examples/browser_player/main.ts",
-		"examples/buffer_playground/main.html",
-		"examples/buffer_playground/main.css",
-		"examples/buffer_playground/main.ts"
-	],
-	bundle: true,
-	minify: true,
-	sourcemap: true,
-	outdir: "build",
-	loader: {".html": "copy"}
-};
-
-if(process.argv[2] == "--build")
-{
-	await esbuild.build(opts);
-}
-
-if(process.argv[2] == "--server")
-{
-	let ctx = await esbuild.context(opts);
-	await ctx.serve({servedir: "build", port: 8080});
-}
-
-if(process.argv[2] == "--prepare" && process.env.INIT_CWD != process.cwd())
-{
-	await esbuild.build(
+	"library":
 	{
 		entryPoints: ["src/agressive_hls.ts", "src/agressive_hls.d.ts"],
 		minify: true,
 		outdir: "build",
 		loader: {".d.ts": "copy"}
-	});
+	},
+	"examples":
+	{
+		entryPoints:
+		[
+			"examples/browser_player/main.html",
+			"examples/browser_player/main.css",
+			"examples/browser_player/main.ts",
+			"examples/buffer_playground/main.html",
+			"examples/buffer_playground/main.css",
+			"examples/buffer_playground/main.ts"
+		],
+		bundle: true,
+		minify: true,
+		sourcemap: true,
+		outdir: "build",
+		loader: {".html": "copy"}
+	}
+};
+
+if(process.argv.includes("--build-library"))
+{
+	if(process.argv.includes("--only-as-dependency")) if(process.env.INIT_CWD != process.cwd()) process.exit();
+	await esbuild.build(configurations.library);
+	process.exit();
+}
+
+if(process.argv.includes("--build-examples"))
+{
+	await esbuild.build(configurations.examples);
+	process.exit();
+}
+
+if(process.argv.includes("--server"))
+{
+	let ctx = await esbuild.context(configurations.examples);
+	await ctx.serve({servedir: "build", port: 8080});
 }
